a29153723534702159b6d0ec16bb2b0d43e23880
gabrielperuzzaufla <39596472+Space-G@users.noreply.github.com>
2019-11-22 11:27:48 -0300

    + Conserta erro de restrição de foreign key em create_tables.sql

--- Views/create_tables.sql
+++ Views/create_tables.sql
@@ -18,32 +18,31 @@ CREATE TABLE user(
 
 CREATE TABLE user_fetishes(
     id INT(6) UNSIGNED PRIMARY KEY,
-    FOREIGN KEY (id) REFERENCES [user] (id) ON DELETE CASCADE,
+    FOREIGN KEY (id) REFERENCES user(id) ON DELETE CASCADE,
     fetish SET('Afeto', 'Anal (ativo)', 'Anal (passivo)', 'Vanilla', 'Carinho pós-coito', 'Fisting', 'Pegging',
         'Bondage', 'Privação sensorial', 'Sadismo', 'Masoquismo', 'Espancamento leve', 'RP geral', 'Diferença de idade',
         'Dominação', 'Submissão', 'Mestre/Escravo', 'Pet play', 'Humilhação', 'Corte', 'Perfuração', 'Sufocamento',
-        'Desmaio', 'Golden Shower', 'Escatologia') NULL;
-);
+        'Desmaio', 'Golden Shower', 'Escatologia') NULL);
 
 CREATE TABLE requests(
-    client_id INT(6) NOT NULL,
-    escort_id INT(6) NOT NULL,
+    client_id INT(6) UNSIGNED NOT NULL,
+    escort_id INT(6) UNSIGNED NOT NULL,
     PRIMARY KEY (client_id, escort_id),
     FOREIGN KEY (client_id) REFERENCES user(id) ON DELETE CASCADE,
     FOREIGN KEY (escort_id) REFERENCES user(id) ON DELETE CASCADE
 );
 
 CREATE TABLE friends(
-     client_id INT(6) NOT NULL,
-     escort_id INT(6) NOT NULL,
+     client_id INT(6) UNSIGNED NOT NULL,
+     escort_id INT(6) UNSIGNED NOT NULL,
      PRIMARY KEY (client_id, escort_id),
      FOREIGN KEY (client_id) REFERENCES user(id) ON DELETE CASCADE,
      FOREIGN KEY (escort_id) REFERENCES user(id) ON DELETE CASCADE
 );
 
 CREATE TABLE ratings(
-    rater_id INT(6) NOT NULL,
-    subject_id INT(6) NOT NULL,
+    rater_id INT(6) UNSIGNED NOT NULL,
+    subject_id INT(6) UNSIGNED NOT NULL,
     score DECIMAL(1) NOT NULL,
     PRIMARY KEY (rater_id, subject_id),
     FOREIGN KEY (rater_id) REFERENCES user(id) ON DELETE CASCADE,
