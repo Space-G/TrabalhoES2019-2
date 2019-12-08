f9e2d3c22935aaf574a74ff63026a7abb24155df
Space-G <39596472+Space-G@users.noreply.github.com>
2019-12-08 20:35:35 -0300

    + Adiciona regionsDAO.php como parte da reestruturação para MCV

---
+++ Persistence/regionsDAO.php
@@ -0,0 +1,28 @@
+<!--Responsável pelo CRUD da tabela regions-->
+<?php
+class regionsDAO{
+	function __construct(){}
+
+//	cria uma nova região
+	function create_region($estado, $regiao, $connection){
+		$new_region = $connection->prepare("INSERT INTO regions(estado, regiao) VALUES (?, ?)");
+		return $new_region->execute(array($estado, $regiao));
+	}
+
+//	seleciona todas regiões
+	function get_region($connection){
+		return $connection->query("SELECT * FROM regions");
+	}
+
+//	edita região de ID selecionado
+	function update_region($region_id, $estado, $regiao, $connection){
+		$region_stmt = $connection->prepare("UPDATE regions SET estado = ?, regiao = ? WHERE region_id = ?");
+		return $region_stmt->execute(array($estado, $regiao, $region_id));
+	}
+
+//	delete região de ID selecionado
+	function delete_region($region_id, $connection){
+		$delete_stmt = $connection->prepare("DELETE FROM regions WHERE region_id = ?");
+		return $delete_stmt->execute(array($region_id));
+	}
+}
