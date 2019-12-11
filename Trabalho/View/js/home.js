77a0645f209627e79f68fa040af9e4e764c30e4d
Space-G <39596472+Space-G@users.noreply.github.com>
2019-12-11 17:35:42 -0300

    + Adiciona listagem de perfil

--- Trabalho/View/js/home.js
+++ Trabalho/View/js/home.js
@@ -3,12 +3,13 @@ $.post('../../Controller/is_logged_in.php').done(function(data){
 	if(!response['logged_in']) {
 		document.location.href = 'login.html';
 	} else{
-		document.cookie = ("profile_id=" + response['user_id']);
+		document.cookie = ("user_id=" + response['user_id']);
 		secret(response['user_id']);
 	}
 });
 
 function my_profile(){
+	document.cookie = ("profile_id=" + getCookie('user_id'));
 	document.location.href = 'perfil.html';
 }
 
@@ -23,3 +24,18 @@ function secret(user_id) {
 		});
 }
 
+function getCookie(cname) {
+	var name = cname + "=";
+	var decodedCookie = decodeURIComponent(document.cookie);
+	var ca = decodedCookie.split(';');
+	for (var i = 0; i < ca.length; i++) {
+		var c = ca[i];
+		while (c.charAt(0) == ' ') {
+			c = c.substring(1);
+		}
+		if (c.indexOf(name) == 0) {
+			return c.substring(name.length, c.length);
+		}
+	}
+	return "";
+}
---
+++ Trabalho/View/js/listagem.js
@@ -0,0 +1,27 @@
+$.post("../../Controller/list_profiles.php", {}).done(function(data){
+	let response = JSON.parse(data)[0];
+	// console.log(response);
+	for(let i = 0; i < response.length; i++){
+		let aux_str = "<a onclick='check_profile(" + response[i]['id'] + ")'><div class='inline'>\n" +
+			"<div style='background-image: url(../img/" + response[i]['picture_file'] + ");background-size: cover;background-repeat: no-repeat;width: 145px;height: 145px;background-position: center;'></div>\n" +
+			"<p>" + response[i]['name'] + "</p>\n" +
+			"<p class='simbolo'>";
+
+		if(response[i]['gender'].toLowerCase() == "masculino"){
+			aux_str = aux_str +'♂ ';
+		}else if (response[i]['gender'].toLowerCase() == "feminino"){
+			aux_str = aux_str + '♀ ';
+		}else{
+			aux_str = aux_str + '⚲ ';
+		}
+
+		aux_str = aux_str +  response[i]['gender_identity'] + " </p>\n" +
+			"</div></a>";
+		$("#colocar-perfis-aqui").append(aux_str);
+	}
+});
+
+function check_profile(id){
+	document.cookie = ("profile_id=" + id);
+	document.location.href = 'perfil.html';
+}
