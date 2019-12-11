e46edcb876778b5e2f3d3d4e1df88f168773ddb9
Space-G <39596472+Space-G@users.noreply.github.com>
2019-12-11 17:22:19 -0300

    + Finaliza CRUD de 3 tabelas pro ADMIN

--- Trabalho/Controller/update_date.php
+++ Trabalho/Controller/update_date.php
@@ -10,5 +10,9 @@ $db = $db->conectar();
 $datesDAO = new datesDAO();
 if($_POST['func'] == 'add') {
 	print_r($datesDAO->create_date($_POST['add_date_id1'], $_POST['add_date_id2'], $_POST['add_date_data'], $_POST['add_date_regiao'], $db));
+}elseif ($_POST['func'] == 'update'){
+	print_r($datesDAO->update_date($_POST['date_id'], $_POST['user_id1'], $_POST['user_id2'], $_POST['date_regiao'], $_POST['date_data'], $db));
+} else{
+	print_r($datesDAO->delete_date($_POST['date_id'], $db));
 }
 ?>
--- Trabalho/View/js/date.js
+++ Trabalho/View/js/date.js
@@ -57,12 +57,9 @@ function get_regions(){
 
 function preencher_coisas(){
 	let response = joao;
-	console.log("drugs")
 	for(let i = 0; i < response.length; i++) {
 		let this_response = response[i];
 		let aux_str = "#edit_date_regiao_" + this_response['id'] +" > option";
-		// let aux_str = "#edit_date_regiao_6 > option";
-		console.log(aux_str);
 		$(aux_str).each(function(){
 			if($(this).val() == this_response['region']){
 				$(this).attr('selected', 'selected');
@@ -73,10 +70,33 @@ function preencher_coisas(){
 
 var joao;
 
+function atualizar_date(id) {
+	$.post("../../Controller/update_date.php", {
+		func: 'update',
+		date_id: id,
+		user_id1: $("#edit-date-user1-" + id).val(),
+		user_id2: $("#edit-date-user2-" + id).val(),
+		date_data: $("#edit-date-data-" + id).val(),
+		date_regiao: $("#edit_date_regiao_" + id).val()
+	}).done(function (data) {
+		console.log(data);
+		location.reload(true);
+	});
+}
+
+function remover_date(id){
+	$.post("../../Controller/update_date.php", {
+		func: 'delete',
+		date_id: id
+	}).done(function (data) {
+		console.log(data);
+		location.reload(true);
+	});
+}
+
 $.post("../../Controller/get_dates.php", {}).done(function(data){
 	let response = JSON.parse(data);
 	joao = response;
-	console.log(response);
 	for(let i = 0; i < response.length; i++){
 		let this_response = response[i];
 		let aux_str = "<form id='edit-date-" + this_response['id'] + "'>\n" +
@@ -84,13 +104,17 @@ $.post("../../Controller/get_dates.php", {}).done(function(data){
 			"<input type='text' maxlength='4' minlength='1' id='edit-date-user2-" + this_response['id'] + "' style='width: 2em' placeholder='ID' value='" + this_response['user2'] + "'/>\n" +
 			"<input type='date' id='edit-date-data-" + this_response['id'] + "' value='" + this_response['day'] + "'/>\n" +
 			"<select id='edit_date_regiao_" + this_response['id'] + "' name='region-dropdown'>\n" +
-			"</select><div>\n" +
-			"<button class='botao botao-medio' onclick='atualizar_date(" + this_response['id'] + ")' id='atualizarDate'>Atualizar date</button>\n" +
-			"<button class='botao botao-medio' onclick='remover_date(" + this_response['id'] + ")' id='removerDate'>Remover date</button>\n" +
-			"</div>" +
-			"</form>";
+			"</select></form><div>\n" +
+			"<button class='botao botao-medio' onclick='atualizar_date(" + this_response['id'] + ")' id='atualizar_date_" + this_response['id'] + "'>Atualizar date</button>\n" +
+			"<button class='botao botao-medio' onclick='remover_date(" + this_response['id'] + ")' id='remover_date_" + this_response['id'] + "'>Remover date</button>\n" +
+			"</div>";
 		$("#div_date").append(aux_str);
 	}
 	get_regions();
+
+	// favor ignorar
+	setTimeout(preencher_coisas, 200);
+	setTimeout(preencher_coisas, 500);
 	setTimeout(preencher_coisas, 1000);
-})
+	setTimeout(preencher_coisas, 10000);
+});
